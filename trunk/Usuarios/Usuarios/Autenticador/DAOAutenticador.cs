a31e02c453aaf18e7d995b3716d088de99cc095f
oduvaldo.vick <oduvaldo.vick@49be7300-f226-11dd-ac65-f74f249cfaac>
2009-02-20 00:32:17 +0000

    Métodos de Login e VerificaPermissoes feitos Esboço do DAOAutenticador feito

---
+++ trunk/Usuarios/Usuarios/Autenticador/DAOAutenticador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Usuarios.Autenticador
+{
+    public class DAOAutenticador
+    {
+        public DAOAutenticador()
+        {            
+        }
+
+        /// <summary>
+        /// Recebe um identificador de Usuario e um identificador de Tela, e devolve um objeto Autenticador 
+        /// com as permissoes inserir, excluir, consultar e editar daquele Usuario para aquela Tela
+        /// </summary>
+        /// <param name="idUsuario"></param>
+        /// <param name="idTela"></param>
+        /// <returns></returns>
+        public Autenticador VerificaPermissoes(int idUsuario, int idTela)
+        {
+            
+        }
+    }    
+}
