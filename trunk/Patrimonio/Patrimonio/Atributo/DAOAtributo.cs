8ba842cfd8dfc1a39d298bb2c5dec71af4753a82
fgutiyama <fgutiyama@49be7300-f226-11dd-ac65-f74f249cfaac>
2009-02-20 03:00:10 +0000

    ARVDatabase class adicionada

--- trunk/Patrimonio/Patrimonio/Atributo/DAOAtributo.cs
+++ trunk/Patrimonio/Patrimonio/Atributo/DAOAtributo.cs
@@ -8,6 +8,8 @@ using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
+using DAL.ARVDatabase;
+using System.Collections.Generic;
 
 namespace Patrimonio.Atributo
 {
@@ -20,27 +22,29 @@ namespace Patrimonio.Atributo
         }
         public int InsereAtributo(Atributo atributo)
         {
-            int linhasafetadas;
-            int idGerado;
+            int linhasafetadas = 0;
+            int idGerado = 0;
             SqlConnection conn = null;
             try
             {
-                conn = new SqlConnection(new ConectorSQL().StringConexao);
-                conn.Open();
-                SqlCommand cmd = new SqlCommand(
-                  " INSERT INTO tb_Atributo (nomeAtributo, descAtributo, tipoAtributo, nuloAtributo) "
-                  + " VALUES     ( "
-                  + " ' " + atributo.Nome + " ',"
-                  + " ' " + atributo.Descricao + " ',"
-                  + " ' " + atributo.Tipo + " ',"
-                  + " ' " + atributo.Nulo + " ')"
-                  , conn);
-                linhasafetadas = cmd.ExecuteNonQuery();
-                SqlCommand cmd2 = new SqlCommand(" SELECT     SCOPE_IDENTITY() AS ID", conn);
-                idGerado =Int32.Parse(cmd2.ExecuteScalar().ToString());
+                ArvDatabase db = new ArvDatabase(new ConectorSQL().StringConexao);
+                List<SqlParameter> parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter("@nome", atributo.Nome));
+                parameters.Add(new SqlParameter("@tipo", atributo.Tipo));
+                parameters.Add(new SqlParameter("@descricao" , atributo.Descricao));
+                parameters.Add(new SqlParameter("@nulo", atributo.Nulo));
                 
+                db.AbreConexao();
+                linhasafetadas = db.ExecuteTextNonQuery(" INSERT INTO tb_Atributo (nomeAtributo, descAtributo, tipoAtributo, nuloAtributo) VALUES( @nome, @descricao, @tipo, @nulo)", parameters);
+                
+                idGerado = Int32.Parse(db.ExecuteScalar(" SELECT SCOPE_IDENTITY() AS ID").ToString());
+
+                db.FechaConexao();
             }
-          
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             finally
             {
                 conn.Close();
