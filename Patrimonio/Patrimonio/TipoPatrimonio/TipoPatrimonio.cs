f30d8a96a34813cdb7ff5b6a91a34f1a278fb739
leandrodvd <leandrodvd@49be7300-f226-11dd-ac65-f74f249cfaac>
2009-02-27 12:48:27 +0000

    Criadas assinaturas dos métodos da DAOTipoPatrimonio (mas pode mudar sequiser) Criada a estrutura do ResponseTipoPatrimonio

--- Patrimonio/Patrimonio/TipoPatrimonio/TipoPatrimonio.cs
+++ Patrimonio/Patrimonio/TipoPatrimonio/TipoPatrimonio.cs
@@ -10,6 +10,26 @@ namespace Patrimonio.TipoPatrimonio
         public TipoPatrimonio()
         { 
         }
-
+        private int _id;
+        [XmlElement(ElementName = "Id")]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _nome;
+        [XmlElement(ElementName = "Nome")]
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        private string _descricao;
+        [XmlElement(ElementName = "Descricao")]
+        public string Descricao
+        {
+            get { return _descricao; }
+            set { _descricao = value; }
+        }
     }
 }
