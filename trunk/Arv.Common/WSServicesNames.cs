18126c8bee54c7340a30794dd18b5fb9f53db04b
Anaue.Costa <Anaue.Costa@49be7300-f226-11dd-ac65-f74f249cfaac>
2009-03-05 10:37:25 +0000

    INCLUSÃO DE UMA NOVA CLASSE. PARA SER UTILIZADO NOS CLIENTS WS

---
+++ trunk/Arv.Common/WSServicesNames.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arv.Common
+{
+    public static class WSServicesNames
+    {
+        #region Usuario
+        public static string USUARIO_CRIAR = "CriarUsuario";
+        public static string USUARIO_DELETAR = "DeletarUsuario";
+        public static string USUARIO_CONSULTAR = "ConsultarUsuario";
+        public static string USUARIO_ALTERAR = "AlterarUsuario";
+        public static string USUARIO_BUSCAR = "BuscarUsuario";
+        #endregion Usuario
+
+        #region TipoPatrimonio
+        public static string TIPO_PATRIMONIO_CRIAR = "CriarTipoPatrimonio";
+        public static string TIPO_PATRIMONIO_DELETAR = "DeletarTipoPatrimonio";
+        public static string TIPO_PATRIMONIO_CONSULTAR = "ConsultarTipoPatrimonio";
+        public static string TIPO_PATRIMONIO_ALTERAR = "AlterarTipoPatrimonio";
+        public static string TIPO_PATRIMONIO_LISTAR = "BuscarTipoPatrimonio";
+        #endregion TipoPatrimonio
+
+        #region Patrimonio
+        public static string PATRIMONIO_CRIAR = "CriarPatrimonio";
+        public static string PATRIMONIO_DELETAR = "DeletarPatrimonio";
+        public static string PATRIMONIO_CONSULTAR = "ConsultarPatrimonio";
+        public static string PATRIMONIO_ALTERAR = "AlterarPatrimonio";
+        public static string PATRIMONIO_LISTAR = "BuscarPatrimonio";
+        #endregion Patrimonio
+
+        #region Autenticador
+        public static string AUTENTICADOR_LOGIN = "Login";
+        public static string AUTENTICADOR_VERIFICA_PERMISSOES = "VerificaPermissoes";
+        #endregion Autenticador
+
+        #region Atributo
+        public static string ATRIBUTO_CRIAR = "CriarAtributo";
+        public static string ATRIBUTO_DELETAR = "DeletarAtributo";
+        public static string ATRIBUTO_CONSULTAR = "ConsultarAtributo";
+        public static string ATRIBUTO_ALTERAR = "AlterarAtributo";
+        public static string ATRIBUTO_BUSCAR = "BuscarAtributo";
+        #endregion Atributo
+
+    }
+}
