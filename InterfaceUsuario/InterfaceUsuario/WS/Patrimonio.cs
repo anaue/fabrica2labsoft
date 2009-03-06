74186a9be38082d7c7eb34e604b0a500f38b7d90
Anaue.Costa <Anaue.Costa@49be7300-f226-11dd-ac65-f74f249cfaac>
2009-03-06 03:42:36 +0000

    Camada de Negocios - Registrar Manutencao

--- InterfaceUsuario/InterfaceUsuario/WS/Patrimonio.cs
+++ InterfaceUsuario/InterfaceUsuario/WS/Patrimonio.cs
@@ -53,16 +53,72 @@ namespace InterfaceUsuario.WS
 
             return _listaPatrimonio;
         }
-        /// <summary>
-        /// Metodo incompleto
-        /// </summary>
-        /// <returns></returns>
-        //public List<Patrimonio> ConsultarPatrimonio(int _IdTipoAtributo)
-        
-        //public List<Patrimonio> ConsultarPatrimonio(int _IdTipoAtributo, int _IdAtributo)
-        
-        //public List<Patrimonio> ConsultarPatrimonio(DateTime DataInicial, DateTime DataFinal)
-        
- 
+
+        public int RegistrarPatrimonio(Classes.Patrimonio patrimonio, Classes.Manutencao manutencao)
+        {
+            int retorno = -1;
+            ServicoPatrimonio.ServicoPatrimonio wsPatrimonio = new InterfaceUsuario.ServicoPatrimonio.ServicoPatrimonio();
+            ServicoPatrimonio.RequestPatrimonio request = new InterfaceUsuario.ServicoPatrimonio.RequestPatrimonio();
+            ServicoPatrimonio.ResponsePatrimonio response = new InterfaceUsuario.ServicoPatrimonio.ResponsePatrimonio();
+
+            #region Acesso WS Diretorio
+            Diretorio dir = new Diretorio();
+            // A nome do serviço é definido no banco de dados, pelo serviço diretório
+            string _url = dir.ObtemEnderecoServico(Arv.Common.WSServicesNames.PATRIMONIO_REGISTRAR_MANUTENCAO);
+            dir = null;
+
+            #endregion Acesso WS Diretorio
+
+            if (_url != string.Empty)
+            {
+                wsPatrimonio.Url = _url;
+                request.Patrimonio.IdEquipamento = patrimonio.IdEquipamento;
+                request.Patrimonio.LocalPatrimonio= patrimonio.LocalPatrimonio;
+                request.Patrimonio.NumeroNotaFiscal = patrimonio.NumeroNotaFiscal;
+                request.Patrimonio.NumeroPECE = patrimonio.NumeroPECE;
+                request.Patrimonio.NumeroPedido = patrimonio.NumeroPedido;
+                request.Patrimonio.CaminhoFotoNotaFiscal = patrimonio.CaminhoFotoNotaFiscal;
+                request.Patrimonio.CaminhoFotoPatrimonio= patrimonio.CaminhoFotoPatrimonio;
+                request.Patrimonio.DataCompra = patrimonio.DtCompra;
+                request.Patrimonio.DataExpGarantia = patrimonio.DtExpGarantia;
+
+                request.Patrimonio.ListAtributos = new InterfaceUsuario.ServicoPatrimonio.Atributo[patrimonio.ListAtributos.Count];
+
+                for (int i = 0; i < patrimonio.ListAtributos.Count; i++)
+                {
+                    request.Patrimonio.ListAtributos[i] = new InterfaceUsuario.ServicoPatrimonio.Atributo();
+                    request.Patrimonio.ListAtributos[i].Descricao = patrimonio.ListAtributos[i].Descricao;
+                    request.Patrimonio.ListAtributos[i].Id = patrimonio.ListAtributos[i].Id;
+                    patrimonio.ListAtributos[i].ListaValores.CopyTo(request.Patrimonio.ListAtributos[i].ListaValores);
+                    request.Patrimonio.ListAtributos[i].Nome = patrimonio.ListAtributos[i].Nome;
+                    request.Patrimonio.ListAtributos[i].Nulo = patrimonio.ListAtributos[i].Nulo;
+                    request.Patrimonio.ListAtributos[i].Tipo = patrimonio.ListAtributos[i].Tipo;
+                    request.Patrimonio.ListAtributos[i].Valor = patrimonio.ListAtributos[i].Valor;
+
+                }
+
+                request.Manutencao = new ServicoPatrimonio.Manutencao();
+                request.Manutencao.IdManutencao = manutencao.IdManutencao;
+                request.Manutencao.IdUsuario= manutencao.IdUsuario;
+                request.Manutencao.Motivo= manutencao.Motivo;
+                request.Manutencao.Observacao = manutencao.Observacao;
+
+                try
+                {
+                    response = wsPatrimonio.RegistrarManutencao(request);
+                    if (response != null && response.StatusCode == InterfaceUsuario.ServicoPatrimonio.ResponseStatus.OK && response.Manutencao != null)
+                        retorno = response.Manutencao.IdManutencao ;
+
+                }
+                catch (Exception ex)
+                {
+                    //necessario mostrar uma mensagem ao usuario
+                    throw ex;
+                }
+            }
+
+            return retorno;
+        }
+     
     }
 }
