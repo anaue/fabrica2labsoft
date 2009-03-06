4d9e4fdf7586f81a52f2123bcdb0b363c43418f6
Anaue.Costa <Anaue.Costa@49be7300-f226-11dd-ac65-f74f249cfaac>
2009-03-06 05:08:12 +0000

    Cria Altera e Deleta Patrimonio - Ws.Patrimonio e Classes.Patrimonio

--- InterfaceUsuario/InterfaceUsuario/WS/Patrimonio.cs
+++ InterfaceUsuario/InterfaceUsuario/WS/Patrimonio.cs
@@ -78,6 +78,7 @@ namespace InterfaceUsuario.WS
                 request.Manutencao.IdUsuario= manutencao.IdUsuario;
                 request.Manutencao.Motivo= manutencao.Motivo;
                 request.Manutencao.Observacao = manutencao.Observacao;
+                request.Manutencao.IdEquipamento = manutencao.IdPatrimonio;
 
                 try
                 {
@@ -150,6 +151,108 @@ namespace InterfaceUsuario.WS
                     //necessario mostrar uma mensagem ao usuario
                     throw ex;
                 }
+            }
+
+            return retorno;
+        }
+
+        public bool DeletaPatrimonio(int id)
+        {
+            bool retorno = false;
+            ServicoPatrimonio.ServicoPatrimonio wsPatrimonio = new InterfaceUsuario.ServicoPatrimonio.ServicoPatrimonio();
+            ServicoPatrimonio.RequestPatrimonio request = new InterfaceUsuario.ServicoPatrimonio.RequestPatrimonio();
+            ServicoPatrimonio.ResponsePatrimonio response = new InterfaceUsuario.ServicoPatrimonio.ResponsePatrimonio();
+
+            #region Acesso WS Diretorio
+            Diretorio dir = new Diretorio();
+            // A nome do serviço é definido no banco de dados, pelo serviço diretório
+            string _url = dir.ObtemEnderecoServico(Arv.Common.WSServicesNames.PATRIMONIO_DELETAR);
+            dir = null;
+            #endregion
+
+            //Properties.Settings.Default.InterfaceUsuario_ServicoAtributo_ServicoAtributo;
+
+            if (_url != string.Empty)
+            {
+                // atualiza o endereco do WS
+                wsPatrimonio.Url = _url;
+
+                request.Patrimonio = new InterfaceUsuario.ServicoPatrimonio.Patrimonio();
+                request.Patrimonio.IdEquipamento = id;
+                try
+                {
+                    response = wsPatrimonio.DeletarPatrimonio(request);
+                    if (response != null && response.StatusCode == InterfaceUsuario.ServicoPatrimonio.ResponseStatus.OK)
+                        retorno = true;
+
+                }
+                catch (Exception ex)
+                {
+                    //necessario mostrar uma mensagem ao usuario
+                    throw ex;
+                }
+
+            }
+            return retorno;
+        }
+
+        public int AlteraPatrimonio(Classes.Patrimonio patrimonio)
+        {
+            int retorno = -1;
+            ServicoPatrimonio.ServicoPatrimonio wsPatrimonio = new InterfaceUsuario.ServicoPatrimonio.ServicoPatrimonio();
+            ServicoPatrimonio.RequestPatrimonio request = new InterfaceUsuario.ServicoPatrimonio.RequestPatrimonio();
+            ServicoPatrimonio.ResponsePatrimonio response = new InterfaceUsuario.ServicoPatrimonio.ResponsePatrimonio();
+
+            #region Acesso WS Diretorio
+            Diretorio dir = new Diretorio();
+            // A nome do serviço é definido no banco de dados, pelo serviço diretório
+            string _url = dir.ObtemEnderecoServico(Arv.Common.WSServicesNames.PATRIMONIO_ALTERAR);
+            dir = null;
+            #endregion
+
+            //Properties.Settings.Default.InterfaceUsuario_ServicoAtributo_ServicoAtributo;
+
+            if (_url != string.Empty)
+            {
+                // atualiza o endereco do WS
+                wsPatrimonio.Url = _url;
+                request.Patrimonio.IdEquipamento = patrimonio.IdEquipamento;
+                request.Patrimonio.LocalPatrimonio = patrimonio.LocalPatrimonio;
+                request.Patrimonio.NumeroNotaFiscal = patrimonio.NumeroNotaFiscal;
+                request.Patrimonio.NumeroPECE = patrimonio.NumeroPECE;
+                request.Patrimonio.NumeroPedido = patrimonio.NumeroPedido;
+                request.Patrimonio.CaminhoFotoNotaFiscal = patrimonio.CaminhoFotoNotaFiscal;
+                request.Patrimonio.CaminhoFotoPatrimonio = patrimonio.CaminhoFotoPatrimonio;
+                request.Patrimonio.DataCompra = patrimonio.DtCompra;
+                request.Patrimonio.DataExpGarantia = patrimonio.DtExpGarantia;
+
+                request.Patrimonio.ListAtributos = new InterfaceUsuario.ServicoPatrimonio.Atributo[patrimonio.ListAtributos.Count];
+
+                for (int i = 0; i < patrimonio.ListAtributos.Count; i++)
+                {
+                    request.Patrimonio.ListAtributos[i] = new InterfaceUsuario.ServicoPatrimonio.Atributo();
+                    request.Patrimonio.ListAtributos[i].Descricao = patrimonio.ListAtributos[i].Descricao;
+                    request.Patrimonio.ListAtributos[i].Id = patrimonio.ListAtributos[i].Id;
+                    patrimonio.ListAtributos[i].ListaValores.CopyTo(request.Patrimonio.ListAtributos[i].ListaValores);
+                    request.Patrimonio.ListAtributos[i].Nome = patrimonio.ListAtributos[i].Nome;
+                    request.Patrimonio.ListAtributos[i].Nulo = patrimonio.ListAtributos[i].Nulo;
+                    request.Patrimonio.ListAtributos[i].Tipo = patrimonio.ListAtributos[i].Tipo;
+                    request.Patrimonio.ListAtributos[i].Valor = patrimonio.ListAtributos[i].Valor;
+                }
+
+                try
+                {
+                    response = wsPatrimonio.AlterarPatrimonio(request);
+                    if (response != null && response.StatusCode == InterfaceUsuario.ServicoPatrimonio.ResponseStatus.OK && response.ListaPatrimonio != null)
+                        retorno = response.ListaPatrimonio[0].IdEquipamento;
+
+                }
+                catch (Exception ex)
+                {
+                    //necessario mostrar uma mensagem ao usuario
+                    throw ex;
+                }
+
             }
 
             return retorno;
